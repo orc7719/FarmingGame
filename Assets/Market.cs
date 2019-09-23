80446c2765f8a6404f38f20d723c8cfd00f07bed
orC7719 <orc7719@gmail.com>
2019-09-24 00:04:30 +0100

    Added Crop Market

--- Assets/Market.cs
+++ Assets/Market.cs
@@ -2,7 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Market : MonoBehaviour
+public class Market : MonoBehaviour, IInteractable
 {
+    public List<Crop> cropOrder = new List<Crop>();
 
+
+    public void Interact()
+    {
+        if (PlayerItem.Instance.CurrentItem is Crop)
+        {
+            if (cropOrder.Contains((Crop)PlayerItem.Instance.CurrentItem))
+            {
+                cropOrder.Remove((Crop)PlayerItem.Instance.CurrentItem);
+                PlayerItem.Instance.DestroyItem();
+
+                CheckCurrentOrder();
+            }
+        }
+    }
+
+    void CheckCurrentOrder()
+    {
+
+    }
+
+    public bool isInteractable()
+    {
+        return true;
+    }
 }
