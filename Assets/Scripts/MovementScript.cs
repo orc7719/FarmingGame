0d44d946d2f2b231c23253a6205bcfae59720ebd
Adam Gwenter <boffinbox@hotmail.co.uk>
2019-09-23 17:45:55 +0100

    basic movement working, linked to time and frames rather than distance and speed.

--- Assets/Scripts/MovementScript.cs
+++ Assets/Scripts/MovementScript.cs
@@ -6,7 +6,7 @@ public class MovementScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
-    {  
+    {
     }
     // Update is called once per frame
     void Update()
@@ -14,12 +14,43 @@ public class MovementScript : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             Debug.Log("left click detected");
-            this.transform.position = Get2DMousePosition();
+            StopCoroutine("CharacterMovement");
+            StartCoroutine("CharacterMovement");
         }
     }
     Vector2 Get2DMousePosition()
     {
         Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         return new Vector2(pos.x, pos.y);
+    }
+    IEnumerator CharacterMovement()
+    {
+        Vector2 targetPosition = Get2DMousePosition();
+        Vector2 originalPosition = transform.position;
+        Vector2 directionVector = targetPosition - originalPosition;
+        while (!ProximityCheck(transform.position, targetPosition))
+        {
+            transform.position = AddVector(transform.position, (directionVector/100));
+            yield return null;
+        }
+        yield return null;
+    }
+    bool ProximityCheck(Vector2 a, Vector2 b)
+    {
+        if (Vector2.Distance(a, b) < 0.01f)
+        {
+            return true;
+        }
+        else
+        {
+            Debug.Log("proximity check failed");
+            return false;
+        }
+    }
+    Vector2 AddVector(Vector2 subject, Vector2 delta)
+    {
+        float newX = subject.x + delta.x;
+        float newY = subject.y + delta.y;
+        return new Vector2(newX, newY);
     }
 }
