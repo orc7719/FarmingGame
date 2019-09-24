5422fe981807816b4930afb3981451a60110cc1a
Adam Gwenter <boffinbox@hotmail.co.uk>
2019-09-24 14:35:04 +0100

    reduced the movement script down to only what is necessary, now that the pathfinding project is in place

--- Assets/Scripts/MovementScript.cs
+++ Assets/Scripts/MovementScript.cs
@@ -5,60 +5,21 @@ using Pathfinding;
 
 public class MovementScript : MonoBehaviour
 {
-    float speed = 0.02f;
     AIPath ai;
-    // Start is called before the first frame update
     void Start()
     {
         ai = GetComponent<AIPath>();
     }
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            Debug.Log("left click detected");
             ai.destination = Get2DMousePosition();
-            //StopCoroutine("CharacterMovement");
-            //StartCoroutine("CharacterMovement");
         }
     }
     Vector2 Get2DMousePosition()
     {
         Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         return new Vector2(pos.x, pos.y);
-    }
-    IEnumerator CharacterMovement()
-    {
-        Vector2 targetPosition = Get2DMousePosition();
-        Vector2 originalPosition = transform.position;
-        Vector2 directionVector = targetPosition - originalPosition;
-        directionVector.Normalize();
-        while (!ProximityCheck(transform.position, targetPosition))
-        {
-            transform.position = AddVector(transform.position, (directionVector * speed));
-            yield return null;
-        }
-        yield return null;
-    }
-    bool ProximityCheck(Vector2 a, Vector2 b)
-    {
-        // We make the proximity check twice the speed because this gives a few frames for the check to hit
-        // Not the best approach, I know.
-        if (Vector2.Distance(a, b) < (speed * 2))
-        {
-            return true;
-        }
-        else
-        {
-            Debug.Log("proximity check failed");
-            return false;
-        }
-    }
-    Vector2 AddVector(Vector2 subject, Vector2 delta)
-    {
-        float newX = subject.x + delta.x;
-        float newY = subject.y + delta.y;
-        return new Vector2(newX, newY);
     }
 }
