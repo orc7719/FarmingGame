7af154a5090f6d21d65166ff7ed535a1fa96ea37
Adam Gwenter <boffinbox@hotmail.co.uk>
2019-09-24 15:28:58 +0100

    movement animation added by rotating the player sprite slightly whenever ai pathing is detected

---
+++ Assets/Scripts/MovementBobbleScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class MovementBobbleScript : MonoBehaviour
+{
+    AIPath ai;
+    float bobbleRate = 1f;
+    int angleToBobble = 10;
+    bool haventRecentlyReset = true;
+    void Start()
+    {
+        ai = GetComponentInParent<AIPath>();
+    }
+    void Update()
+    {
+        if ((ai.velocity.magnitude > 0) && !ai.reachedDestination)
+        {
+            if (!haventRecentlyReset)
+            {
+                haventRecentlyReset = true;
+            }
+            Vector3 currentEuler = transform.localEulerAngles;
+            float z = currentEuler.z;
+            if (z > angleToBobble)
+            {
+                if (z < (360 - angleToBobble))
+                {
+                    bobbleRate *= -1;
+                }
+            }
+            z += bobbleRate;
+            transform.localRotation = Quaternion.Euler(new Vector3(0, 0, z));
+        }
+        if (ai.reachedDestination && haventRecentlyReset)
+        {
+            ResetRotation();
+            haventRecentlyReset = false;
+        }
+    }
+    void ResetRotation()
+    {
+        Debug.Log("reset spam");
+        transform.localRotation = Quaternion.Euler(0, 0, 0);
+    }
+}
