298391bb8fe84143a1390d399fd27c6df68f1788
Adam Gwenter <boffinbox@hotmail.co.uk>
2019-09-25 17:59:57 +0100

    added a "touch to continue" blinking text when the tutorial is finished

---
+++ Assets/Scripts/FadeInOutScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class FadeInOutScript: MonoBehaviour
+{
+	float lerpValue = 0.0f;
+	float lerpValueChange = 0.005f;
+	Color lerp1;
+	Color lerp2;
+	// Use this for initialization
+	void Start()
+	{
+		lerp1 = new Color (1,1,1,1);
+		lerp2 = new Color (1,1,1,0);
+	}
+	// Update is called once per frame
+	void Update()
+	{
+		this.gameObject.GetComponent<TMPro.TextMeshProUGUI>().color = Color.Lerp(lerp1, lerp2, lerpValue);
+		lerpValue = lerpValue + lerpValueChange;
+		if (lerpValue > 0.98f)
+		{
+			lerpValueChange = -0.02f;
+		}
+		if (lerpValue < 0.02f)
+		{
+			lerpValueChange = 0.02f;
+		}
+	}
+}
