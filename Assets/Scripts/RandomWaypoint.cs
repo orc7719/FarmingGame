b84559f1546a00d08653478a5ff296fe6c33057d
Adam Gwenter <boffinbox@hotmail.co.uk>
2019-09-26 17:21:30 +0100

    menu background system complete. a random waypoint is chosen every 6 seconds for the fake player to bounce towards.

---
+++ Assets/Scripts/RandomWaypoint.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomWaypoint : MonoBehaviour
+{
+    float timeUntilNextWaypoint = 0;
+    private void Update()
+    {
+        timeUntilNextWaypoint -= Time.deltaTime;
+        if (timeUntilNextWaypoint < 0)
+        {
+            timeUntilNextWaypoint = 6f;
+            transform.position = new Vector3(Random.Range(-3.5f, +2.8f), Random.Range(-1.95f, +1.95f), 0);
+        }
+    }
+}
