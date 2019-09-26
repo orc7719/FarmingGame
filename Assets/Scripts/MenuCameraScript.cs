0a570da50556b253c92c8b4cfee7a194db3a03d7
Adam Gwenter <boffinbox@hotmail.co.uk>
2019-09-26 15:14:39 +0100

    set up more of the animated menu background - the fake player paths towards a target transform that can be repositioned around the scene in script

---
+++ Assets/Scripts/MenuCameraScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuCameraScript : MonoBehaviour
+{
+    [SerializeField] Transform player;
+    float elasticity = 2f;
+    int z;
+    // Start is called before the first frame update
+    void Start()
+    {
+        z = Mathf.RoundToInt(transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3((player.position.x / elasticity), (player.position.y / elasticity), z);
+    }
+}
