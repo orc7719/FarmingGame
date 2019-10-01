aebb3a003a481d5c941b02f4c5073051f29a69a0
Cory McAdam <orc7719@gmail.com>
2019-10-01 14:32:08 +0100

    Setup Level Template & Tilemaps

---
+++ Assets/ResizeCamera.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[ExecuteInEditMode]
+public class ResizeCamera : MonoBehaviour
+{
+
+    // Set this to the in-world distance between the left & right edges of your scene.
+    public float sceneWidth = 10;
+
+    Camera cam;
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    // Adjust the camera's height so the desired scene width fits in view
+    // even if the screen/window size changes dynamically.
+    void Update()
+    {
+        if(cam== null)
+            cam = GetComponent<Camera>();
+
+        float unitsPerPixel = sceneWidth / Screen.width;
+
+        float desiredHalfHeight = 0.5f * unitsPerPixel * Screen.height;
+
+        cam.orthographicSize = desiredHalfHeight;
+    }
+}
